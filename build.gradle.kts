c48c477b022a5c0926abaafa3f585c0c13e503fd
Riichan04 <22130274@st.hcmuaf.edu.vn>
2026-05-04 00:17:57 +0700

    chore: config libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ description = "TMDT_BookingMakeup_App"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
@@ -20,13 +20,39 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-    testCompileOnly("org.projectlombok:lombok")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testAnnotationProcessor("org.projectlombok:lombok")
+    runtimeOnly(libs.postgresql)
+
+    api(libs.org.jdbi.jdbi3.core)
+    api(libs.org.jdbi.jdbi3.sqlobject)
+
+    api(libs.org.projectlombok.lombok)
+    annotationProcessor(libs.org.projectlombok.lombok)
+
+    api(libs.com.google.code.gson.gson)
+
+    api(libs.org.mindrot.jbcrypt)
+
+    //For Hibernate/JPA ORM
+    api(libs.org.hibernate.orm)
+
+    api(libs.jsoup)
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    implementation (libs.cloudinary)
+    implementation (libs.cloudinary.http5)
+
+    api(libs.spring.boot.starter.mail)
+    implementation(libs.spring.boot.starter.thymeleaf)
+    implementation(libs.springboot.starter)
+    implementation(libs.spring.boot.starter.data.jpa)
+//    implementation(libs.spring.boot.starter.data.redis)
+    implementation(libs.spring.boot.starter.web)
+
+    testImplementation(libs.org.junit.jupiter.junit.jupiter.api)
+    testImplementation(libs.org.junit.jupiter.junit.jupiter.engine)
 }
 
 tasks.withType<Test> {
