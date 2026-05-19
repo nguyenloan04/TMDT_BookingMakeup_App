58c22d848bd5b85c39eefe88316bb1f926f051e0
Loan Nguyen <22130148@st.hcmuaf.edu.vn>
2026-05-19 20:37:46 +0700

    Config Cloudinary (completed)

---
+++ src/main/java/com/example/tmdt_bookingmakeup_app/services/CloudinaryService.java
@@ -0,0 +1,47 @@
+package com.example.tmdt_bookingmakeup_app.services;
+
+import com.cloudinary.Cloudinary;
+import com.example.tmdt_bookingmakeup_app.config.CloudinaryConfig.CloudinaryProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    private final Cloudinary cloudinary;
+    private final CloudinaryProperties props;
+
+    @Autowired
+    public CloudinaryService(Cloudinary cloudinary, CloudinaryProperties props) {
+        this.cloudinary = cloudinary;
+        this.props = props;
+    }
+
+    public Map<String, Object> generateSignature(String folderName, String fileName, String contentType) {
+        try {
+            if (folderName == null) return null;
+
+            Map<String, Object> listUrlParams = new HashMap<>(Map.of(
+                    "timestamp", System.currentTimeMillis() / 1000L,
+                    "folder", folderName,
+                    "public_id", fileName)
+            );
+
+            /**
+             * Signature Version param value:
+             *  1 = SHA-1
+             *  2 = SHA-256
+             */
+            String signature = cloudinary.apiSignRequest(listUrlParams, props.getSecret(), 2);
+            listUrlParams.put("signature", signature);
+            listUrlParams.put("api_key", props.getApiKey());
+
+            return listUrlParams;
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+}
