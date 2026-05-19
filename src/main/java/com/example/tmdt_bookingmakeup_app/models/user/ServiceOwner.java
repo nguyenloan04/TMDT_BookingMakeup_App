f5a3e95cfe6b610c340b472084bc8fd0c9b22795
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2026-05-19 10:17:50 +0700

    load data for search page

--- src/main/java/com/example/tmdt_bookingmakeup_app/models/user/ServiceOwner.java
+++ src/main/java/com/example/tmdt_bookingmakeup_app/models/user/ServiceOwner.java
@@ -29,8 +29,7 @@ public class ServiceOwner {
     @Column(name = "experience_years")
     private Integer experienceYears;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "showcase_type", columnDefinition = "TEXT")
+    @Column(name = "showcase_type")
     private ShowcaseType showcaseType;
 
     @Column(name="identify_front")
